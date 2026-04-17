e7c07f1ab15c7f1c2ad1f487da8bd56efa40aafd
tanavishali52 <tanavish.Ali@consultancyoutfit.co.uk>
2026-04-17 12:09:31 +0500

    chore: configure Vercel rewrites and Vite build settings for client deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ ADD THIS LINE
   plugins: [react()],
   resolve: {
     alias: {
